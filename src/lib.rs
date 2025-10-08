743d246e064c8e5533f7831e8305b0b8a86a4f79
Taylor Schneider <taylorschneider07@yahoo.com>
2025-10-08 12:24:42 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 //#![deny(missing_docs)]
 /// The `extra` module organizes the types and additional functions involved with the use of Posits
 pub mod utils;
-pub mod extra;
 pub mod certum;
+
+#[cfg(test)]
+pub mod tests;
 // pub mod dimid;
 // pub mod quarta;
 // pub mod acute;
@@ -11,3 +13,8 @@ pub use certum::certum8::c8;
 pub use certum::certum16::c16;
 pub use certum::certum32::c32;
 pub use certum::certum64::c64;
+
+pub use certum::u_certum8::uc8;
+pub use certum::u_certum16::uc16;
+pub use certum::u_certum32::uc32;
+pub use certum::u_certum64::uc64;
