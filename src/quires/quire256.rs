6a67377e9a674cc5dda042590c93176ada5f391e
Taylor Schneider <taylorschneider07@yahoo.com>
2025-10-28 16:13:37 -0400

    Update quire256.rs

--- src/quires/quire256.rs
+++ src/quires/quire256.rs
@@ -16,14 +16,14 @@ impl Add for u256 {
     type Output = Self;
     fn add(self, rhs: Self) -> Self {
         let over_lower = u128::overflowing_add(self.bits.0, rhs.bits.0);
-        let partial_lower;
+        // let partial_lower;
         let carry;
         if over_lower.1 {
             
         }
         let sum_upper = u128::saturating_add(u128::saturating_add(self.bits.1, rhs.bits.1), carry);
-        let sum_lower = u128::saturating_add(self, rhs)
-
+        let sum_lower = u128::saturating_add(self.bits.0, rhs.bits.0);
+        Self { bits: (sum_lower, sum_upper) }
     }
 }
 
